41c67124883622a3fa494f5c592d0d680d367c78
Oliwia Wądołowska <oliwia.wadolowska@gmail.com>
2024-12-06 18:33:27 +0100

    basic setup

---
+++ scala-station/src/main/scala/Main.scala
@@ -0,0 +1,21 @@
+import org.apache.pekko
+import pekko.actor.{ActorSystem, Actor, ActorLogging, ActorRef, Props}
+
+
+case object Oskar
+
+class MovieStar extends Actor with ActorLogging {
+  def receive: Receive = {
+    case Oskar =>
+      log.info("Oskar? No i świetnie! Idę na emeryturę!")
+      context.system.terminate()
+    case msg => log.info(s"Odebrałem wiadomość: ${msg}")
+  }
+}
+
+@main
+def mainProg: Unit = {
+  val system = ActorSystem("Hollywood")
+  val leonardo = system.actorOf(Props[MovieStar](), "leonardo")
+  leonardo ! Oskar
+}
